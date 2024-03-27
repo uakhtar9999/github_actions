749667be36ed8772cc07590fc6581f211c62ef28
Umair Akhtar <uakhtar9999@gmail.com>
2024-03-27 11:25:16 +0530

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,9 +5,9 @@ provider "azurerm" {
 # Define Terraform backend configuration
 terraform {
   backend "azurerm" {                             # Use AzureRM backend for storing Terraform state
-    resource_group_name = "Tanzu"                 # Azure Resource Group where Terraform state is stored
-    storage_account_name = "amitdevopspipeline"   # Name of the Azure Storage Account for Terraform state
-    container_name = "statefile"                  # Name of the Azure Storage Container for Terraform state
+    resource_group_name = "Terraform-POC"                 # Azure Resource Group where Terraform state is stored
+    storage_account_name = "terraformpocdev01"   # Name of the Azure Storage Account for Terraform state
+    container_name = "tfstate"                  # Name of the Azure Storage Container for Terraform state
     key = "terraform.tfstate"                     # Key or path to the Terraform state file within the container
   }
 }
