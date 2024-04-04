8101d8cb8f4dbdece02bc1ff1125c6c746f25c63
Umair Akhtar <uakhtar9999@gmail.com>
2024-04-04 13:14:11 +0530

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,8 +13,7 @@ terraform {
 }
 
 # module "RG" {
-#   source                = "./modules/azurerm_resource_group"
-#   count                 = var.create_resource_group ? 1 : 0
+#   source                = "../Terraform/resource_group"
 #   resource_group_name   = var.resource_group_name
 #   location              = var.location
 #   create_resource_group = var.create_resource_group
---
+++ Terraform/resource_group/output.tf
@@ -0,0 +1,4 @@
+output "id" {
+  value       = "${azurerm_resource_group.main.id}"
+  description = "The resource group ID"
+}
